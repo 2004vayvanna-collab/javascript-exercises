bb46ec14aeab9fe92f0b1609e9db6bcd283fb365
2004vayvanna-collab <2004vayvanna-collab@users.noreply.github.com>
2026-01-18 18:41:18 +0100

    Complete joinStrings exercise

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -1,19 +1,17 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+// Step 1: Define the variables
+const firstName = "Carlos"
+const lastName = "Stevenson"
+const thisYear = 1965
+const birthYear = 1947
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
+// Step 2: Create fullName and age
+const fullName = firstName + " " + lastName
+const age = thisYear - birthYear
 
-// Add your code right below, good luck!
+// Step 3: Construct the greeting
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
 
-
-
-
-
-// Do not change this
+// Do not change below this line
 module.exports = {
   firstName: typeof firstName === 'undefined' ? undefined : firstName,
   lastName: typeof lastName === 'undefined' ? undefined : lastName,
@@ -23,3 +21,4 @@ module.exports = {
   fullName: typeof fullName === 'undefined' ? undefined : fullName,
   age: typeof age === 'undefined' ? undefined : age
 }
+
